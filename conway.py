5dd292c4367f838156ed9e6f7f2ee19a4b7ebd16
Florian Fusseder <fussederflorian@gmail.com>
2022-12-12 10:22:43 +0100

    add controls

--- conway.py
+++ conway.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from enum import Enum
 
@@ -14,33 +15,50 @@ class State(Enum):
     dead = (10, 10, 40)
 
 
-def init_rand(col, row):
-    return np.random.randint(0, 2, size=(col, row))
+class Controls:
 
+    def __init__(self, surface, window_width, window_height, size) -> None:
+        self.surface = surface
+        self.offset = size[0] + 5
 
-def init_slider(col, row):
-    matrix = np.zeros((col, row))
-    pattern = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-                         0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-                         0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                         0, 0, 1, 1, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                         0, 0, 1, 1, 0, 0, 0],
-                        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                         0, 0, 0, 0, 0, 0, 0],
-                        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-                         0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-                         0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                         0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                         0, 0, 0, 0, 0, 0, 0]])
-    pos = (3, 3)
-    matrix[pos[0]:pos[0] + pattern.shape[0], pos[1]:pos[1] + pattern.shape[1]] = pattern
-    return matrix
+        self.pos_x = window_width - self.offset * 4
+        self.pos_y = window_height
+
+        self.play_pos = (self.pos_x, self.pos_y)
+        self.pause_pos = (self.pos_x + self.offset, self.pos_y)
+        self.next_pos = (self.pos_x + 2 * self.offset, self.pos_y)
+        self.refresh_pos = (self.pos_x + 3 * self.offset, self.pos_y)
+
+        def init_button(path, pos, _size):
+            return pygame.Rect(pos[0], pos[1], _size[0], _size[1]), pygame.transform.scale(pygame.image.load(path), _size)
+
+        self.play_rect, self.play_icon = init_button(os.path.join("play.png"), self.play_pos, size)
+        self.pause_rect, self.pause_icon = init_button(os.path.join("pause.png"), self.pause_pos, size)
+        self.next_rect, self.next_icon = init_button(os.path.join("angle-right.png"), self.next_pos, size)
+        self.refresh_rect, self.refresh_icon = init_button(os.path.join("refresh.png"), self.refresh_pos, size)
+
+    def draw(self):
+        pygame.draw.rect(self.surface, (255, 255, 255), self.next_rect)
+        pygame.draw.rect(self.surface, (255, 255, 255), self.play_rect)
+        pygame.draw.rect(self.surface, (255, 255, 255), self.pause_rect)
+        pygame.draw.rect(self.surface, (255, 255, 255), self.refresh_rect)
+
+        self.surface.blit(self.play_icon, self.play_pos)
+        self.surface.blit(self.next_icon, self.next_pos)
+        self.surface.blit(self.pause_icon, self.pause_pos)
+        self.surface.blit(self.refresh_icon, self.refresh_pos)
+
+    def next_clicked(self, event):
+        return event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.next_rect.collidepoint(event.pos)
+
+    def pause_clicked(self, event):
+        return event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.pause_rect.collidepoint(event.pos)
+
+    def refresh_clicked(self, event):
+        return event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.refresh_rect.collidepoint(event.pos)
+
+    def play_clicked(self, event):
+        return event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.play_rect.collidepoint(event.pos)
 
 
 @click.command()
@@ -50,40 +68,63 @@ def init_slider(col, row):
 def conway(cell_size, col, row):
     pygame.init()
     pygame.display.set_caption("John Conway's Game of Life")
-    button = pygame.Rect(10, 10, 50, 50)
+    width = col * cell_size + 1
+    height = row * cell_size + 1
 
-    surface = pygame.display.set_mode((col * cell_size + 1, row * cell_size + 1))
-    matrix = init_rand(col, row)
+    surface = pygame.display.set_mode((width, height + 50))
+    controls = Controls(surface, width, height, (50, 50))
 
-    surface.fill(grid_color)
-    for x, y in np.ndindex(matrix.shape):
-        pygame.draw.rect(surface, State.alive.value if matrix[x, y] else State.dead.value,
-                         pygame.Rect(x * cell_size + 1, y * cell_size + 1, cell_size - 1, cell_size - 1))
-
+    matrix = generate_start(cell_size, col, row, surface)
+    controls.draw()
     pygame.display.update()
 
+    run_game = False
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and button.collidepoint(event.pos):
-                surface.fill(grid_color)
-                matrix_new = np.zeros((col, row), dtype=int)
+            elif controls.next_clicked(event):
+                matrix = next_generation(cell_size, col, matrix, row, surface)
+            elif controls.refresh_clicked(event):
+                matrix = generate_start(cell_size, col, row, surface)
+                run_game = False
+            elif controls.pause_clicked(event):
+                run_game = False
+            elif controls.play_clicked(event):
+                run_game = True
 
-                for x, y in np.ndindex(matrix.shape):
-                    alive_neighbours = np.sum(matrix[x - 1:x + 2, y - 1:y + 2]) - matrix[x, y]
+        if run_game:
+            matrix = next_generation(cell_size, col, matrix, row, surface)
 
-                    if matrix[x, y] == 1 and alive_neighbours < 2 or alive_neighbours > 3:
-                        matrix_new[x, y] = 0
-                    elif alive_neighbours == 3 or (
-                            matrix[x, y] == 1 and (2 == alive_neighbours or alive_neighbours == 3)):
-                        matrix_new[x, y] = 1
+        controls.draw()
+        pygame.display.update()
 
-                    pygame.draw.rect(surface, State.alive.value if matrix_new[x, y] else State.dead.value,
-                                     pygame.Rect(x * cell_size + 1, y * cell_size + 1, cell_size - 1, cell_size - 1))
-                pygame.display.update()
-                matrix = matrix_new
+
+def next_generation(cell_size, col, matrix, row, surface):
+    surface.fill(grid_color)
+    matrix_new = np.zeros((col, row), dtype=int)
+    for x, y in np.ndindex(matrix.shape):
+        alive_neighbours = np.sum(matrix[x - 1:x + 2, y - 1:y + 2]) - matrix[x, y]
+
+        if matrix[x, y] == 1 and alive_neighbours < 2 or alive_neighbours > 3:
+            matrix_new[x, y] = 0
+        elif alive_neighbours == 3 or (
+                matrix[x, y] == 1 and (2 == alive_neighbours or alive_neighbours == 3)):
+            matrix_new[x, y] = 1
+
+        pygame.draw.rect(surface, State.alive.value if matrix_new[x, y] else State.dead.value,
+                         pygame.Rect(x * cell_size + 1, y * cell_size + 1, cell_size - 1, cell_size - 1))
+    return matrix_new
+
+
+def generate_start(cell_size, col, row, surface):
+    matrix = np.random.randint(0, 2, size=(col, row))
+    surface.fill(grid_color)
+    for x, y in np.ndindex(matrix.shape):
+        pygame.draw.rect(surface, State.alive.value if matrix[x, y] else State.dead.value, pygame.Rect(x * cell_size + 1, y * cell_size + 1, cell_size - 1, cell_size - 1))
+
+    return matrix
 
 
 if __name__ == "__main__":
